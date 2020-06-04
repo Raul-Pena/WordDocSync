608fe03aad43b4a3204fc00396f37b3f52a2854e
Raul-Pena <rnpena0805@gmail.com>
2020-06-04 12:11:43 -0500

    script push

--- test script.bat
+++ test script.bat
@@ -2,12 +2,12 @@
 
 set /p str1=Enter name of file: 
 
-quit
 
 pandoc %str1% -f docx -t gfm -o %str1:.docx=.md%
+pandoc %str1% -f docx -t markdown -o %str1:.docx=2.md%
 set /p message=Would you like to add a change description?:
 
 git add *
-git commit -m "committing changes to Doc.md"
+git commit -m "script push"
 
 pause
